d3e113106322ac0029c05b0de45eb6215647abaf
SantoshChapagai <you@example.com>
2025-12-25 14:49:30 +0200

    create a yml file for github workflows

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,17 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
+    coverage: {
+      include: ["src/**/*.{ts,tsx}"],
+      provider: "v8",
+      reporter: ["text", "json-summary", "json"],
+      reportOnFailure: true,
+      thresholds: {
+        lines: 60,
+        branches: 60,
+        functions: 60,
+        statements: 60,
+      },
+    },
   },
 });
